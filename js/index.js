b7d48cf4a536c914019cc3c2f7ac623dad3c5e50
funnydeiv <funnydeiv@gmail.com>
2019-11-14 19:31:08 +0000

    hometask'

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let isOperLast=0;
+const display = document.querySelector('.calc .display');
+document.querySelectorAll('.digits')
+    .forEach(button =>button.addEventListener('click',digitPressed));
+
+function digitPressed(event){
+    const btnText = event.target.innerHTML;
+    isOperLast = 0;
+    display.value+=btnText;
+}
+
+document.querySelectorAll('.opers')
+    .forEach(button =>button.addEventListener('click',operPressed));
+
+function operPressed(event){
+    if (isOperLast ===0){
+        const btnText = event.target.innerHTML;
+        isOperLast = 1;
+        display.value+=btnText;
+    }
+}   
+
+document.querySelector('.calc .eq')
+    .addEventListener('click',eqPressed);
+
+function eqPressed(){
+    display.value = eval(display.value);
+}
+
+document.querySelector('.calc .clear')
+    .addEventListener('click',clearPressed);
+
+function clearPressed(){
+    isOperLast =0;
+    display.value ='';
+}

03b39a21b9368e911dfc501f28e9c4e0d4d8241a
funnydeiv <48495272+funnydeiv@users.noreply.github.com>
2019-11-15 17:35:02 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,15 +13,15 @@ document.querySelectorAll('.opers')
     .forEach(button =>button.addEventListener('click',operPressed));
 
 function operPressed(event){
-    const btnText = event.target.innerHTML;
-    if (isOperLast ===0){
+    if (isOperLast ===0){  
+        const btnText = event.target.innerHTML;
         isOperLast = 1;
+        display.value+=btnText;
     } else { 
+        const btnText = event.target.innerHTML;
         let dispVal = display.value;
-        display.value = dispVal.substr(0, dispVal.length-1); 
+        display.value = dispVal.substr(0, dispVal.length-1) + btnText;  
     }
-    display.value+=btnText;
-    isOperLast=1;
 }   
 
 document.querySelector('.calc .eq')
